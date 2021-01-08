2b15201fa2e1fc59dc4977f7470a1d36d6079f4b
Rodrigo Eustaquio Duque <rodrigoeduque@hotmail.com>
2021-01-08 14:06:44 -0300

    Formulario - Criar Produto - Concluido

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,6 +20,11 @@ import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component'
 import { MatButtonModule } from "@angular/material/button";
 
+import { HttpClientModule } from "@angular/common/http";
+
+import { FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -41,7 +46,11 @@ import { MatButtonModule } from "@angular/material/button";
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
-import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { ProductService } from './../product.service';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -9,17 +10,34 @@ import { Router } from "@angular/router";
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor(private productService:ProductService, private router:Router) { }
+  product: Product = {
+    name: '',
+    price: null
+  }
+
+  constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
-    
+
   }
 
-  createProduct():void{
-    this.productService.showMessage('Produto Criado')
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto Criado')
+      this.router.navigate(['/products'])
+
+      //teste console log inserção
+
+            console.log('Produto Adicionado!')
+            console.log('código: ' + this.product.id)
+            console.log('name: ' + this.product.name)
+            console.log('price: ' + this.product.price)
+      
+    })
+
   }
 
-  cancel():void{
+  cancel(): void {
     this.productService.showMessage('Cancelando')
     this.router.navigate(['/products'])
   }
